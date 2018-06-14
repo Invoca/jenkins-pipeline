a6b9e3e2fe2ececb60c9cfe14e13748e0ec48448
Christian Parkinson <cparkinson@invoca.com>
2018-06-14 18:08:48 -0400

    OPS-1932: GitHubStatus fixes

--- src/com/invoca/github/GitHubStatus.groovy
+++ src/com/invoca/github/GitHubStatus.groovy
@@ -2,47 +2,45 @@
 package com.invoca.github
 
 import java.net.HttpURLConnection
-import java.net.URI
+import java.net.*
+import groovy.json.JsonOutput
 
 class GitHubStatus implements Serializable {
   final static String GITHUB_API_URL_TEMPLATE = "https://api.github.com/repos/%s/statuses/%s"
 
+  private String context
+  private String description
   private String repoSlug
   private String sha
+  private String status
   private String targetURL
-  private String description
-  private String context
+  private String token
 
-  GitHubStatus(Map config) {
-    this.repoSlug = config.repoSlug
-    this.sha = config.sha
-    this.targetURL = config.targetURL
-    this.description = config.description
-    this.context = config.context
-    this.status = config.status
-  }
-
-  boolean update {
-    HttpURLConnection connection = (HttpURLConnection) URL().openConnection()
+  boolean update() {
+    HttpURLConnection connection = (HttpURLConnection) gitHubURL().openConnection()
     connection.setRequestMethod("POST")
     connection.setRequestProperty("Content-Type", "application/json")
-    connection.getOutputStream().write(payload())
+    connection.setRequestProperty("Authorization", "token " + token)
+    connection.setDoOutput(true)
+
+    OutputStreamWriter writer = new OutputStreamWriter(connection.getOutputStream(), "UTF-8")
+    writer.write(payload())
 
     connection.getResponseCode() == HttpURLConnection.HTTP_CREATED
   }
 
-  private String URL() {
-    new URL(String.format(GITHUB_API_URL_TEMPLATE, repoSlug, sha))
+  private URL gitHubURL() {
+    new URI(String.format(GITHUB_API_URL_TEMPLATE, repoSlug, sha)).toURL()
   }
 
   private String payload(String state) {
-    payload = [
+    def payload = [
       state: status,
       target_url: targetURL,
       description: description,
       context: context
     ]
 
-    new JSONObject(payload).toString()
+    JsonOutput.toJson(payload)
   }
 }
