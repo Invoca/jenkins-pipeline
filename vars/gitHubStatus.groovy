f40e35e47f050848431e029492ca45e92834b6e3
James Ebentier <jebentier@invoca.com>
2018-07-10 13:05:31 -0700

    TECH-2610: extract out GitHubApi into its own class

--- vars/gitHubStatus.groovy
+++ vars/gitHubStatus.groovy
@@ -1,6 +1,6 @@
 import com.invoca.github.GitHubStatus
 
 def call(Map<String, Object> config) {
-  def github = new GitHubStatus(this, config.repoSlug, config.sha, config.targetURL, config.token)
-  github.setStatus(config.context, config.status, config.description)
+  config.script = this
+  GitHubStatus.update(config)
 }
