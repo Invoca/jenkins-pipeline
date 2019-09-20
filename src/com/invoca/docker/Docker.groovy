ece50af9a9bf12e32a35b3a75bba93a3197a7cfe
Jack Chistyakov <jchistyakov@invoca.com>
2019-09-20 16:02:22 -0700

    TECH-3734: add docker helpers

--- src/com/invoca/docker/Docker.groovy
+++ src/com/invoca/docker/Docker.groovy
@@ -3,3 +3,21 @@ package com.invoca.docker
 def hubLogin(String username, String password) {
   sh "docker login -u ${username} -p ${password}"
 }
+
+def imageExistsLocally(String imageWithTag) {
+  // Will return empty string if no matching image is found
+  return (sh "docker images -q ${imageWithTag}").trim() == ""
+}
+
+def downloadImage(String imageWithTag) {
+  sh "docker pull ${imageWithTag}"
+}
+
+def getImage(String imageWithTag) {
+  // Try downloading image from remote if not found locally.
+  // Return true if image is now present locally, false otherwise.
+  if (!imageExistsLocally(imageWithTag)) {
+    downloadImage(imageWithTag)
+  }
+  return imageExistsLocally(imageWithTag)
+}
