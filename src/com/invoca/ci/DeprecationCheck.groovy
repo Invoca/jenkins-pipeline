313c5a62fcd5e06125432a6ef879b79d1b9912e6
James Ebentier <jebentier@invoca.com>
2020-06-10 17:01:20 +0200

    TECH-4468: update method to run a command and capture the output

--- src/com/invoca/ci/DeprecationCheck.groovy
+++ src/com/invoca/ci/DeprecationCheck.groovy
@@ -4,14 +4,21 @@ package com.invoca.ci
 
 import com.invoca.github.GitHubStatus
 
-def deprecationCheck(String testOutput, Map<String, Object> githubStatusConfig) {
+def runWithDeprecationWarningCheck(String script, Map<String, Object> githubStatusConfig) {
+  def testOutput = sh(returnStdout: true, script: "${script} 2>&1")
+  echo testOutput
+
+  githubStatusConfig.context = 'deprecation-warning-check'
+  githubStatusConfig.script  = this
+
   if (testOutput.contains("Unexpected Deprecation Warnings Encountered")) {
-    githubStatusConfig.status = 'failure'
+    githubStatusConfig.status      = 'failure'
+    githubStatusConfig.description = 'Unexpected deprecation warnings encountered'
   } else {
-    githubStatusConfig.status = 'success'
+    githubStatusConfig.status      = 'success'
+    githubStatusConfig.description = 'No unexpected deprecation warnings encountered'
   }
 
-  githubStatusConfig.script = this
   GitHubStatus.update(githubStatusConfig)
 
   return githubStatusConfig.status == 'success'
