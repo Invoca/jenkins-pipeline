4527d6626a1b9e03c9c443396c3c2c21f2e141d9
James Ebentier <jebentier@invoca.com>
2020-06-18 17:14:09 +0200

    TECH-4468: put the method inside static class

--- src/com/invoca/ci/DeprecationCheck.groovy
+++ src/com/invoca/ci/DeprecationCheck.groovy
@@ -4,22 +4,24 @@ package com.invoca.ci
 
 import com.invoca.github.GitHubStatus
 
-def runWithDeprecationWarningCheck(String script, Map<String, Object> githubStatusConfig) {
-  def testOutput = sh(returnStdout: true, script: "${script} 2>&1")
-  echo testOutput
+class DeprecationCheck {
+  static boolean runWithDeprecationWarningCheck(String script, Map<String, Object> githubStatusConfig) {
+    def testOutput = sh(returnStdout: true, script: "${script} 2>&1")
+    echo testOutput
 
-  githubStatusConfig.context = 'deprecation-warning-check'
-  githubStatusConfig.script  = this
+    githubStatusConfig.context = 'deprecation-warning-check'
+    githubStatusConfig.script  = this
 
-  if (testOutput.contains("Unexpected Deprecation Warnings Encountered")) {
-    githubStatusConfig.status      = 'failure'
-    githubStatusConfig.description = 'Unexpected deprecation warnings encountered'
-  } else {
-    githubStatusConfig.status      = 'success'
-    githubStatusConfig.description = 'No unexpected deprecation warnings encountered'
+    if (testOutput.contains("Unexpected Deprecation Warnings Encountered")) {
+      githubStatusConfig.status      = 'failure'
+      githubStatusConfig.description = 'Unexpected deprecation warnings encountered'
+    } else {
+      githubStatusConfig.status      = 'success'
+      githubStatusConfig.description = 'No unexpected deprecation warnings encountered'
+    }
+
+    GitHubStatus.update(githubStatusConfig)
+
+    return githubStatusConfig.status == 'success'
   }
-
-  GitHubStatus.update(githubStatusConfig)
-
-  return githubStatusConfig.status == 'success'
 }
