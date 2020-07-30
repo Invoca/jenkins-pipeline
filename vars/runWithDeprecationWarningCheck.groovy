53c80cb5ff1ceb496d64caa2b40b3f5b0b46a7f4
Drew Caddell <dcaddell@invoca.com>
2020-07-29 22:24:36 -0700

    TECH-4570: Write test output to file and archive it

--- vars/runWithDeprecationWarningCheck.groovy
+++ vars/runWithDeprecationWarningCheck.groovy
@@ -1,9 +1,12 @@
 import com.invoca.ci.DeprecationWarnings
 
 def call(String script, Map<String, Object> githubStatusConfig) {
-    def testOutput = sh(returnStdout: true, script: "${script} 2>&1")
-    echo testOutput
+  def testOutput = sh(returnStdout: true, script: "${script} 2>&1")
+  echo testOutput
 
-    githubStatusConfig.script = this
-    DeprecationWarnings.checkAndUpdateGithub(testOutput, githubStatusConfig)
-  }
+  writeFile file: 'deprecation_warnings.txt', text: testOutput
+  archiveArtifacts 'deprecation_warnings.txt'
+
+  githubStatusConfig.script = this
+  DeprecationWarnings.checkAndUpdateGithub(testOutput, githubStatusConfig)
+}
