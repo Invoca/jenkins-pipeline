b861b2679b0901a61fbe3fc6a38840d9afcdbab0
James Ebentier <jebentier@invoca.com>
2020-10-15 18:53:58 +0200

    No-Jira: create new cache helpers for jenkins

---
+++ vars/cacheRestore.groovy
@@ -0,0 +1,35 @@
+/**
+ * Helper method for restoring cached artifacts
+ *
+ * @param
+*/
+
+void call(String s3Bucket, ArrayList<String> cacheKeys) {
+  // Verify that aws-cli is installed before proceeding
+  sh 'which aws'
+
+  echo 'Pulling cache from AWS'
+  Boolean cacheExists = false
+  cacheKeys.each { cacheKey ->
+    String cacheTarball = "${cacheKey}.tar.gz"
+    String cacheLocation = "s3://${s3Bucket}/jenkins_cache/${env.JOB_NAME.replaceAll("\\W", "")}/${cacheTarball}"
+
+    cacheExists = sh(script: "aws s3 ls ${cacheLocation}", returnStatus: true) == 0
+    if (cacheExists) {
+      sh "aws s3 cp ${cacheLocation} ${cacheTarball} --content-type application/x-gzip"
+      break;
+    }
+  }
+
+  if (cacheExists) {
+    echo 'Unpacking cache tarball'
+    sh "tar -xzvf ${cacheTarball} ${itemsToCache.join(' ')}"
+
+    echo 'Cleaning up local cache tarball'
+    sh "rm -rf ${cacheTarball}"
+
+    echo 'Cache restored'
+  } else {
+    echo 'Unable to find cache stored for any of the provided keys'
+  }
+}
