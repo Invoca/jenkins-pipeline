91339cc842d24483de535fb6120ab962f7577b1e
James Ebentier <jebentier@invoca.com>
2020-10-19 16:18:21 +0200

    No-Jira: update cacheStore to take an array of keys to push to (optimization)

--- vars/cacheStore.groovy
+++ vars/cacheStore.groovy
@@ -4,18 +4,20 @@
  * @param
 */
 
-void call(String s3Bucket, String cacheKey, ArrayList<String> itemsToCache) {
-  String cacheTarball = "${cacheKey}.tar.gz"
-  String cacheLocation = "s3://${s3Bucket}/jenkins_cache/${env.JOB_NAME.replaceAll("\\W", "")}/${cacheTarball}"
+void call(String s3Bucket, ArrayList<String> cacheKeys, ArrayList<String> itemsToCache) {
+  String cacheTarball = "${cacheKeys[0]}.tar.gz"
 
   // Verify that aws-cli is installed before proceeding
   sh 'which aws'
 
   echo 'Creating local cache tarball'
-  sh "tar -czvf ${cacheTarball} ${itemsToCache.join(' ')}"
+  sh "tar -czf ${cacheTarball} ${itemsToCache.join(' ')}"
 
   echo 'Pushing cache to AWS'
-  sh "aws s3 cp ${cacheTarball} ${cacheLocation} --content-type application/x-gzip"
+  cacheKeys.each { cacheKey ->
+    String cacheLocation = "s3://${s3Bucket}/jenkins_cache/${env.JOB_NAME.replaceAll("\\W", "")}/${cacheKey}.tar.gz"
+    sh "aws s3 cp ${cacheTarball} ${cacheLocation} --content-type application/x-gzip"
+  }
 
   echo 'Cleaning up local cache tarball'
   sh "rm -rf ${cacheTarball}"
